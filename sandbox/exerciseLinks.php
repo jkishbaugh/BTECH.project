aaf46cf403fafb94839ff26b8e40f38c6598a21e
jkishbaugh <kishju@gmail.com>
2019-07-10 07:00:51 -0600

    minor changes

--- sandbox/exerciseLinks.php
+++ sandbox/exerciseLinks.php
@@ -1,4 +1,6 @@
-<?php include(SHARED_PATH."/header.php"); ?>
+<?php
+    require_once ('../private/initialize.php');
+    include(SHARED_PATH."/header.php"); ?>
 
     <h2>PHP Exercises</h2>
     <section class="linkList">
@@ -10,6 +12,7 @@
             <li><a href="userDefinedFunctionsAssignment.php">User Defined Functions Assignment </a></li>
             <li><a href="debugging.php">Debugging Assignment </a></li>
             <li><a href="buildWebPages/links.php">Build Website Assignment </a></li>
+            <li><a href="workingWithForms/index.php">Working with forms Assignment </a></li>
         </ul>
 
     <button><a href="../public/index.html">BACK TO HOME</a></button>
