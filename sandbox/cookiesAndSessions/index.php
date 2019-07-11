78a4832286eb332e57b5ecd5966951997fa5ac06
jkishbaugh <kishju@gmail.com>
2019-07-10 18:17:23 -0600

    logic changes to single page form

---
+++ sandbox/cookiesAndSessions/index.php
@@ -0,0 +1,45 @@
+<?php
+    require_once('../../private/initialize.php');
+
+    //set default value of variables for initial page  load
+    if (!isset($investment)) { $investment = ''; } 
+    if (!isset($interest_rate)) { $interest_rate = ''; } 
+    if (!isset($years)) { $years = ''; }
+
+    $page =6;
+?> 
+
+    <?php include(SHARED_PATH.'/header.php')?>
+    <main>
+    <h1>Future Value Calculator</h1>
+    <?php if (!empty($error_message)) { ?>
+        <p class="error"><?php echo htmlspecialchars($error_message); ?></p>
+    <?php } ?>
+    <form action="display_results.php" method="post">
+
+        <div id="data">
+            <label>Investment Amount:</label>
+            <input type="text" name="investment"
+                   value="<?php echo htmlspecialchars($investment); ?>">
+            <br>
+
+            <label>Yearly Interest Rate:</label>
+            <input type="text" name="interest_rate"
+                   value="<?php echo htmlspecialchars($interest_rate); ?>">
+            <br>
+
+            <label>Number of Years:</label>
+            <input type="text" name="years"
+                   value="<?php echo htmlspecialchars($years); ?>">
+            <br>
+        </div>
+
+        <div id="buttons">
+            <label>&nbsp;</label>
+            <input type="submit" value="Calculate"><br>
+        </div>
+
+    </form>
+        <button class = 'back'><a href='../exerciseLinks.php'>BACK</a></button>
+    </main>
+<?php  include(SHARED_PATH.'/footer.php')?>
--- sandbox/workingWithForms/single_page_form.php
+++ sandbox/workingWithForms/single_page_form.php
@@ -1,47 +1,7 @@
 <?php
 require_once('../../private/initialize.php');
-    $error_message='';
-    if(!isset($_GET['id'])){
-        header(url_for('/sandbox/workingWithForms/index.php'));
-    }
-    $id = $_GET['id'];
 
-if(isPostRequest()) {
-    $investment = filter_input(INPUT_POST, 'investment',
-        FILTER_VALIDATE_FLOAT);
-    $interest_rate = filter_input(INPUT_POST, 'interest_rate',
-        FILTER_VALIDATE_FLOAT);
-    $years = filter_input(INPUT_POST, 'years',
-        FILTER_VALIDATE_INT);
-// validate investment
-    if ( $investment === NULL || $investment === FALSE ) {
-        $error_message = 'Investment must be a valid number.'; }
-    else if ( $investment <= 0 ) {
-        $error_message = 'Investment must be greater than zero.'; }
-
-// validate interest rate
-    else if ( $interest_rate === NULL || $interest_rate === FALSE ) {
-        $error_message = 'Interest rate must be a valid number.'; }
-    else if ( $interest_rate <= 0 ) {
-        $error_message = 'Interest rate must be greater than zero.'; }
-    else if ($interest_rate >15){
-        $error_message = 'Interest rate is too high please enter a lower rate';
-    }
-
-// validate years
-    else if ( $years === NULL || $years === FALSE ) {
-        $error_message = 'Number of years must be a valid whole number.'; }
-    else if ( $years <= 0  ) {
-        $error_message = 'Number of years must be greater than zero.'; }
-    else if ( $years >31 ){
-        $error_message = 'Number must be less than 31';
-    }
-
-// set error message to empty string if no invalid entries
-    else {
-        $error_message = ''; }
-}
-//set default value of variables for initial page load
+    //set default value of variables for initial page load
     if (!isset($investment)) {
         $investment = '';
     }
@@ -52,14 +12,55 @@ if(isPostRequest()) {
         $years = '';
     }
 
+    $error_message='';
+
+
+if(isPostRequest()) {
+    if(!empty($_POST['investment'])){
+        $investment = filter_input(INPUT_POST, 'investment',
+            FILTER_VALIDATE_FLOAT);
+        if ( $investment <= 0 || !is_numeric($investment)) {
+            $error_message = 'Investment must be greater than zero.'; }
+    }else{
+        $error_message = "Investment cannot be left blank";
+    }
+    if(!empty($_POST['interest_rate'])){
+        $interest_rate = filter_input(INPUT_POST, 'interest_rate',
+            FILTER_VALIDATE_FLOAT);
+        if(!is_numeric($interest_rate)|| $interest_rate <=0){
+            $error_message = "Interest rate must be a number greater than 0";
+        }else if($interest_rate > 15){
+            $error_message = "Interest rate cannot be more than 15 ";
+        }
+    }else{
+        $error_message = "Interest rate must have a value entered";
+    }
+    if(!empty($_POST['years'])){
+        $years = filter_input(INPUT_POST, 'years',
+            FILTER_VALIDATE_INT);
+        if(!is_numeric($years)|| $years <= 0){
+            $error_message ="Years must be a number greater than 0";
+        }else if ($years > 30){
+            $error_message = "Years cannot be greater than 30";
+        }
+    }else{
+        $error_message = "Years must have a value entered";
+    }
+}
+
 ?>
+
+
 <?php include(SHARED_PATH.'/header.php');?>
 <main>
     <h1>Future Value Calculator</h1>
+    <?php if ($error_message != ''|| isPostRequest()) {
+    ?>
     <?php if (!empty($error_message)) { ?>
+
         <p class="error"><?php echo htmlspecialchars($error_message); ?></p>
     <?php } ?>
-    <form action="<?php echo url_for('/sandbox/workingWithForms/single_page_form.php?id='.h(u($id)));?>" method="post">
+    <form action="single_page_form.php" method="post">
 
         <div id="data">
             <label>Investment Amount:</label>
@@ -80,12 +81,38 @@ if(isPostRequest()) {
 
         <div id="buttons">
             <label>&nbsp;</label>
-            <input type="submit" value="Calculate"><br>
+            <input type="submit" name="submit" value="Calculate"><br>
+
+            <button class='back'><a href='../exerciseLinks.php'>BACK</a></button>
         </div>
 
     </form>
+    <?php
+        }else{
+            $futureValue = '';
+            for($i = 1;$i<=$years;$i++){
+                $futureValue = $futureValue + ($futureValue * $interest_rate *.01);
+            }
+    // format numbers
+    $investment_formatted = '$'.number_format($investment, 2);
+    $yearly_rate_formatted = $interest_rate.'%';
+    $future_value_formatted = '$'.number_format($future_value, 2);
+    ?>
+    <label>Investment Amount:</label>
+    <span><?php echo $investment_formatted; ?></span><br>
 
-    <button class = 'back'><a href='../exerciseLinks.php'>BACK</a></button>
+    <label>Yearly Interest Rate:</label>
+    <span><?php echo $yearly_rate_formatted; ?></span><br>
+
+    <label>Number of Years:</label>
+    <span><?php echo $years; ?></span><br>
+
+    <label>Future Value:</label>
+    <span><?php echo $future_value_formatted; ?></span>
+    <?php
+        }
+    ?>
 
 </main>
-<?php include(SHARED_PATH.'/footer.php');?>
+<?php include(SHARED_PATH . '/footer.php'); ?>
+
