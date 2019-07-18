8213a61a872e73ab4de357f704454d3c88023ff2
jkishbaugh <kishju@gmail.com>
2019-07-18 15:55:22 -0600

    Final (contact form send email)

--- public/contact.php
+++ public/contact.php
@@ -29,35 +29,7 @@
 
  include("../Shared/header.php");
     ?>
-  <header>
-    <div class="navbar">
-      <nav>
-        <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="faq.html">FAQ</a></li>
-          <li><a href="contact.php">Contact</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li><a href="#openModal">Login</a></li>
-        </ul>
-      </nav>
-      <div id='openModal' class="modalDialog">
-        <div><a href="#close" title="Close" class="close">X</a>
-            <!-- content for Modal -->
-            <div class="login">
-                <h2>Sign in</h2>
-                <form>
-                    <input type="email" name="email" placeholder="Email">
-                    <input type="password" name="password" placeholder="Password">
-                    <input type="button" value="Login">
-                </form>
-            </div><!-- end login -->
-        </div><!-- end close -->
-      </div>
-      <!--end openModal -->
-    </div>
-    <!--end navbar -->
-  </header>
+
   <section id="splash">
     <img src="../img/contact.jpg" alt="Contact Image">
   </section><!--end splash-->
