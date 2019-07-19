0d80ac181396d85a261baeaf40947d5ce932d1c9
jkishbaugh <kishju@gmail.com>
2019-07-18 22:25:09 -0600

    Final (select and display)

--- private/db_creds.php
+++ private/db_creds.php
@@ -2,5 +2,5 @@
     define("DB_SERVER",'localhost');
     define("DB_NAME", 'justinki_graves');
     define("DB_USER", 'webUser');
-    define("DB_PASS", 'dbpass');
+    define("DB_PASS", 'IslandwUser2006HI');
     ?>
