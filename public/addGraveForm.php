785699321a1f03657e8efaa0c784594473ed541b
jkishbaugh <kishju@gmail.com>
2019-07-23 20:01:07 -0600

    Final (select and display)

--- public/addGraveForm.php
+++ public/addGraveForm.php
@@ -27,28 +27,28 @@
         <form class="addAncestor" action="" method="post">
             <div class="w3-row">
                 <div class="w3-half">
-                    <input type="text" name="firstName" placeholder="First Name">
+                    <label>First Name</label>
+                    <input type="text" name="firstName">
                 </div>
                 <div class="w3-half">
-                    <input type="text" name="lastName" placeholder="Last Name">
+                    <label>First Name</label>
+                    <input type="text" name="lastName">
                 </div>
-            </div>
+            </div><!--END ROW -->
             <div class="w3-row">
-                <div class="w3-twothird">
-                    <label>BirthDate</label>
+                <div class="w3-half">
+                    <label>Birth Date</label>
                     <input type="date" name="birthdate" placeholder="Example: 1900-05-05" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}">
                 </div>
-                <div class="w3-third">
-                    <label for="veteran">Veteran
-                        <input type="hidden" name="veteran" value="false">
-                        <input type="checkbox" name="veteran" value="true" placeholder="Veteran">
-                    </label>
-                    <label for="famous">Famous
-                        <input type="hidden" name="famous" value="false">
-                        <input type="checkbox" name="famous" value="true" placeholder="famous">
-                    </label>
+                <div class="w3-half">
+                    <label>Date of Death</label>
+                    <input type="date" name="birthdate" placeholder="Example: 1900-05-05" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}">
                 </div>
-            </div>
+            </div><!--END ROW -->
+            <div class="w3-row">
+                <label>Photo</label>
+                <input type="file" name="photo">
+            </div><!--END ROW -->
 
             <input class="addGrave" type="submit" name="submit" value="Save Record">
 
