8c21d73110567904d1e19601c7c440b72a782cd3
jkishbaugh <kishju@gmail.com>
2019-07-25 17:43:46 -0600

    Final (select and display)

--- private/functions.php
+++ private/functions.php
@@ -131,49 +131,5 @@ function findAncestor($connection, $nameString){
 
 }
 
-function uploadImage($fileToUploadName, $fileToUploadTempName){
-    $target_dir = "../img/uploads/";
-    $target_file = $target_dir . basename($fileToUploadName);
-    $uploadOk = 1;
-    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-    // Check if image file is a actual image or fake image
-    if(isPostRequest()) {
-        $check = getimagesize();
-        if($check !== false) {
-             "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            return "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        return "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        return "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-        return "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        return "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            return "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-            return "Sorry, there was an error uploading your file.";
-        }
-    }
-}
 
 ?>
--- public/addGraveForm.php
+++ public/addGraveForm.php
@@ -5,44 +5,19 @@
 
     if(isPostRequest()){
         //set variables from form
-        $firstName = '';
-        $lastName = '';
-        $birthDate = '';
-        $deathDate = '';
+        $firstName = isset($_POST['firstName'])??'';
+        $lastName = isset($_POST['lastName'])??'';
+        $birthDate = isset($_POST['birthdate'])??'';
+        $deathDate = isset($_POST['birthdate'])??'';
         $photoFileName = $_FILES["fileToUpload"]["name"];
-        $photo= "img\\".$_FILES["name"];
         $tempPhotoFileName = $_FILES["fileToUpload"]["tmp_name"];
+        $photoLocation = "../img/".$photoFileName;
         //validate incoming data
-          if(isset($_POST['firstName'])){
-             $firstName = $_POST['firstName'];
-          }else{
-              $error= "First Name must be included to add a new record";
-          }
 
-           if(isset($_POST['lastName'])){
-               $lastName = $_POST['lastName'];
-           }else{
-               $error= "Last Name must be included to add a new record";
-          }
-            if(isset($_POST['birthdate'])&& $_POST['birthdate']){
-                $birthDate = $_POST['birthdate'];
-            }else{
-                $error= "Birth date must be included to add a new record";
-          }
-
-            if(isset($_POST['birthdate'])&& $_POST['birthdate']){
-                $deathDate = $_POST['birthdate'];
-            }else{
-                $error= "Death date must be included to add a new record";
-            }
-
-
-
+        //upload image
+        //insert new grave
         //reload page and display errors if exist or
-
-            //insert new grave
-            //upload image
-            //redirect if success
+        //redirect if success
 
 
     }
@@ -51,17 +26,13 @@
 
 
     include("../Shared/header.php");
-
-    echo"<h3>". $photoFileName."/ ".$tempPhotoFileName. " / ".$photo."</h3>";
-
     ?>
 
 
     <div class="addAncestorTitle"></div>
     <div>
         <h2>Add Your Ancestor</h2>
-        <form class="addAncestor" action="" method="post" enctype="multipart/form-data">
-            <div class="w3-row">
+        <form class="addAncestor" action="" method="post">
                 <div class="w3-half">
                     <label>First Name</label>
                     <input type="text" name="firstName">
@@ -70,7 +41,6 @@
                     <label>Last Name</label>
                     <input type="text" name="lastName">
                 </div>
-            </div><!--END ROW -->
             <div class="w3-row">
                 <div class="w3-half">
                     <label>Birth Date</label>
@@ -83,7 +53,7 @@
             </div><!--END ROW -->
             <div class="w3-row">
                 <label>Select Image To Upload</label>
-                <input type="file" name="fileToUpload" id="fileToUpload">
+                <input type="file" name="photo">
             </div><!--END ROW -->
 
             <input class="addGrave" type="submit" name="submit" value="Save Record">
