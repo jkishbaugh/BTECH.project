4b4ef9168d65df23d47f5143be4d8a712fc1aa49
jkishbaugh <kishju@gmail.com>
2019-08-29 23:21:26 -0600

    Javascript final

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const IMAGES = document.querySelectorAll("#image");
 
+let growImage = function(e){
+    console.log(e);
 
-IMAGES.forEach(img => img.addEventListener("mouseover",function(){
-    console.log("MouseOver works");
-}));
+}
+IMAGES.forEach(img => img.addEventListener("mouseover",growImage));
